88bb2d142254f6a7d436ef9e03460f4fd6c3ccc7
gogakakabadze <gkaka2017@agruni.edu.ge>
2025-05-20 14:34:24 +0400

    Engineer B updated pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,9 @@
+
 # Configure an ML pipeline, which consists of three stages: tokenizer,
 hashingTF, and lr.
 tokenizer = Tokenizer(inputCol="text", outputCol="words")
 hashingTF = HashingTF(inputCol=tokenizer.getOutputCol(),
  outputCol="features")
-lr = LogisticRegression(maxIter=10, regParam=0.001)
+lr = LogisticRegression(maxIter=model_config["maxIter"],
+ regParam=model_config["regParam"])
 pipeline = Pipeline(stages=[tokenizer, hashingTF, lr])
